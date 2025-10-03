c2f5cadaa6b5440f9657b4cd1c36c56415b68a2b
Aditya Modi <aditya3107modi@gmail.com>
2025-10-03 19:39:38 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const connectDB=async () =>{
+    try {
+        mongoose.connection.on("connected",()=> console.log("Database connected successfully"));
+        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default connectDB;
